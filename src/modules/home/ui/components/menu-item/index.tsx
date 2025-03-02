4a1b01d330323dab2c3c7f13c28a80fc490a6299
homto <homtoyat@gmail.com>
2025-03-02 12:58:36 +0800

    global animation

--- src/modules/home/ui/components/menu-item/index.tsx
+++ src/modules/home/ui/components/menu-item/index.tsx
@@ -1,11 +1,18 @@
+import { cn } from "@/lib/utils";
 import React from "react";
 
 interface Props {
   children: React.ReactNode;
+  className?: string;
 }
-export const MenuItem = ({ children }: Props) => {
+export const MenuItem = ({ children, className = "" }: Props) => {
   return (
-    <div className="text-sm font-semibold  text-white pt-4 flex ">
+    <div
+      className={cn(
+        "text-sm font-semibold  text-white pt-4 flex hover:cursor-pointer  transition-all hover:text-neutral-300 ",
+        className
+      )}
+    >
       {children}
     </div>
   );
--- src/modules/home/ui/components/nav-bar/index.tsx
+++ src/modules/home/ui/components/nav-bar/index.tsx
@@ -4,61 +4,93 @@ import { MenuItem } from "../menu-item";
 import { SearchBar } from "../search-bar";
 
 import {
+  ArrowDown,
   BookAudioIcon,
-  BookDown,
   BookTextIcon,
-  CalendarIcon,
+  ChevronDown,
   ClockIcon,
   CrownIcon,
+  DownloadCloudIcon,
+  DownloadIcon,
   FanIcon,
   FileMusicIcon,
+  HardDriveDownload,
   LucideLightbulb,
   MailIcon,
-  MoonIcon,
   SmileIcon,
   StarsIcon,
   UploadIcon,
   VideoIcon,
-  VideotapeIcon,
 } from "lucide-react";
 import { Button } from "@/components/ui/button";
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Avatar } from "@/components/ui/avatar";
 import Image from "next/image";
 import avatar from "@/../public/avatar.png";
 import banner from "@/../public/banner.png";
+import logo from "@/../public/logo.png";
 import {
   HoverCard,
   HoverCardContent,
   HoverCardTrigger,
 } from "@/components/ui/hover-card";
 import { cn } from "@/lib/utils";
-
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import { Separator } from "@radix-ui/react-context-menu";
+import Link from "next/link";
+import styles from "./index.module.css";
 export const NavBar = () => {
   const [avatarActive, setAvatarActive] = useState(false);
   return (
-    <div className="flex items-center relative   group min-w-[1024px] ">
+    <div className="flex items-center relative   group min-w-[1024px] h-fit">
       <Image
         src={banner}
         alt={"banner"}
         className=" absolute -z-40 group-hover:scale-110 transition-all duration-2000"
       ></Image>
+      <Link href={"/abcs"}>
+        <Image
+          src={logo}
+          alt={"lgo"}
+          width={80}
+          className={cn(
+            "absolute -bottom-1/2 left-32 scale-140 top-14 opacity-50 hover:opacity-100 cursor-pointer transition-all duration-700"
+          )}
+        ></Image>
+      </Link>
+
       <div className="flex gap-2 items-center px-4">
-        <MenuItem>首页</MenuItem>
+        <MenuItem>
+          <div className=" flex items-center">
+            <p>首页</p>
+            <ChevronDown
+              className="hover:rotate-180 transition-all duration-700 "
+              size={20}
+            ></ChevronDown>
+          </div>
+        </MenuItem>
         <MenuItem>番剧</MenuItem>
         <MenuItem>游戏中心</MenuItem>
         <MenuItem>会员购</MenuItem>
         <MenuItem>漫画</MenuItem>
         <MenuItem>赛事</MenuItem>
-        <MenuItem>下载客户端</MenuItem>
+        <MenuItem className="flex flex-row items-center justify-center">
+          <DownloadIcon className="size-4 hover:animate-bounce"></DownloadIcon>
+          下载客户端
+        </MenuItem>
       </div>
       <div className="flex-1">
         <SearchBar></SearchBar>
       </div>
-      <div className="flex gap-4  px-4 items-center">
+      <div className="flex gap-4  px-4 items-center py-2">
         <div className="relative group">
           <Avatar
             className={cn(
-              "bg-red-300 size-14 group transition-all duration-500"
+              "bg-red-300 size-12 group transition-all duration-500 ring-2 ring-neutral-100"
             )}
             onMouseEnter={() => {
               setAvatarActive(true);
@@ -69,11 +101,41 @@ export const NavBar = () => {
         </div>
 
         <MenuItem>
-          <div className="flex flex-col relative items-center">
-            <div className="absolute top-0 right-0 size-2 rounded-full bg-red-600 translate-x-1"></div>
-            <CrownIcon></CrownIcon>
-            <span className="hidden lg:inline">大会员</span>
+          <div className="flex flex-col">
+            <div className=""></div>
+            <Separator></Separator>
           </div>
+
+          <HoverCard openDelay={50} closeDelay={100}>
+            <HoverCardTrigger asChild>
+              <div className="flex flex-col relative items-center group overflow-hidden ">
+                <CrownIcon></CrownIcon>
+                <span className="hidden lg:inline">大会员</span>
+              </div>
+            </HoverCardTrigger>
+            <HoverCardContent
+              className="w-90  mx-0 py-4"
+              sideOffset={5}
+              alignOffset={100}
+              align="center"
+            >
+              <div className="flex flex-col gap-2  text-lg ">
+                <div className="hover:bg-neutral-200 text-neutral-500 transition-all mx-2  rounded  p-2">
+                  回复我的
+                </div>
+                <div className="hover:bg-neutral-200 text-neutral-500 transition-all mx-2  rounded  p-2">
+                  @我的
+                </div>
+                <div className="hover:bg-neutral-200 text-neutral-500 transition-all mx-2  rounded flex  justify-between items-center  p-2">
+                  <p className="inline-block">我的消息</p>
+                  <div className="rounded-full w-2 h-2 bg-red-500"></div>
+                </div>
+                <div className="hover:bg-neutral-200 text-neutral-500 transition-all mx-2  rounded  p-2">
+                  系统消息
+                </div>
+              </div>
+            </HoverCardContent>
+          </HoverCard>
         </MenuItem>
         <MenuItem>
           <div className="flex flex-col relative items-center">
@@ -84,14 +146,18 @@ export const NavBar = () => {
           </div>
         </MenuItem>
         <MenuItem>
-          <div className="flex flex-col justify-center ">
-            <FanIcon></FanIcon>
+          <div className="flex flex-col justify-center group">
+            <div>
+              <FanIcon
+                className={cn("animate-wiggle", " flex items-center")}
+              ></FanIcon>
+            </div>
 
             <span className="hidden lg:inline"> 动态</span>
           </div>
         </MenuItem>
         <MenuItem>
-          <div className="flex flex-col justify-center items-center">
+          <div className={cn("flex flex-col justify-center items-center ")}>
             <StarsIcon></StarsIcon>
 
             <span className="hidden lg:inline"> 收藏</span>
@@ -112,13 +178,21 @@ export const NavBar = () => {
           </div>
         </MenuItem>
         <MenuItem>
-          <HoverCard>
+          <HoverCard openDelay={50} closeDelay={100}>
             <HoverCardTrigger asChild>
-              <Button variant="destructive" className="bg-pink-500">
+              <Button
+                variant="destructive"
+                className="bg-pink-500 hover:cursor-pointer"
+              >
                 <UploadIcon></UploadIcon>投稿
               </Button>
             </HoverCardTrigger>
-            <HoverCardContent className="w-90" sideOffset={5} alignOffset={100}>
+            <HoverCardContent
+              className="w-90 animate-accordion-down"
+              sideOffset={5}
+              alignOffset={100}
+              align="center"
+            >
               <div className="flex justify-between text-neutral-500">
                 <div className="flex flex-col items-center justify-center text-xs p-2 hover:bg-neutral-300 hover:rounded-lg">
                   <BookAudioIcon></BookAudioIcon>
