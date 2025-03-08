1e4e33ee3f445d45ad8f5e6112e5cdb8d42db0a3
homto <homtoyat@gmail.com>
2025-03-08 21:17:12 +0800

    parent variants in motion

--- src/app/(home)/toggle/page.tsx
+++ src/app/(home)/toggle/page.tsx
@@ -1,9 +1,10 @@
 "use client";
 
 import { AnimatePresence, motion } from "framer-motion";
+import { PlusCircleIcon } from "lucide-react";
 import { useState } from "react";
 export default function Motion() {
-  const [visible, setVisible] = useState(true);
+  const [visible, setVisible] = useState(false);
   return (
     <div className="flex flex-col items-center justify-center gap-4">
       <button
@@ -17,12 +18,42 @@ export default function Motion() {
       <AnimatePresence>
         {visible && (
           <motion.div
-            exit={{ opacity: 0, scale: 0.9 }}
-            animate={{ opacity: 1, scale: 1 }}
-            initial={{ opacity: 0, scale: 0 }}
-            className="h-20 w-20 rounded-2xl bg-neutral-700/50 p-4"
+            variants={{
+              open: { opacity: 1, y: "0px", scale: 1 },
+              closed: { opacity: 0, y: "100px", scale: 0.9 },
+            }}
+            exit="closed"
+            animate="open"
+            initial="closed"
+            className="relative h-80 w-80 gap-4 rounded-2xl bg-neutral-700/50 p-4"
           >
-            dddd happy
+            <div className="relative h-[80%] w-full bg-amber-200">
+              <motion.img
+                src="banner.png"
+                className="object-cover"
+                variants={{
+                  open: { opacity: 1, y: "0px", scale: 1 },
+                  closed: { opacity: 0, y: "100px", scale: 0 },
+                }}
+              ></motion.img>
+            </div>
+            <div>BAMBI</div>
+            <motion.button
+              variants={{
+                open: {
+                  scale: [0.38, 1, 0.38],
+                  rotate: "90deg",
+                },
+                closed: {
+                  scale: [1.33, 10, 0.1],
+                  rotate: "-90deg",
+                },
+              }}
+              className="text-pin absolute top-4 right-4"
+              transition={{ duration: 3 }}
+            >
+              <PlusCircleIcon className="rotate-45"></PlusCircleIcon>
+            </motion.button>
           </motion.div>
         )}
       </AnimatePresence>
