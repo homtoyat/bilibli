a03e6395978e2afce67efb785e460d88726ad748
homto <homtoyat@gmail.com>
2025-03-08 17:46:30 +0800

    style useMotionValue

--- src/app/(home)/style/page.tsx
+++ src/app/(home)/style/page.tsx
@@ -1,23 +1,20 @@
 "use client";
-import { motion } from "framer-motion";
+import { motion, useMotionValue } from "framer-motion";
 export default function Motion() {
+  const scale = useMotionValue(3.2);
   return (
     <div className="flex h-dvh flex-col items-center justify-center bg-neutral-100">
       <div className="h-48 w-24 overflow-hidden rounded-2xl bg-yellow-800 text-white">
         <div className="fade-out relative overflow-hidden">
           <motion.div
             className="text-8 fade-out inset-1 flex h-23 origin-top-left flex-wrap text-6xl tracking-wide break-all text-white"
-            animate={{
-              opacity: [0, 1, 0.3],
-
-              scaleX: [1, 3, 5, 6],
-              scaleY: [1, 1, 1, 5],
+            style={{
+              scale,
             }}
             transition={{
               repeat: Infinity,
               repeatDelay: 1,
               duration: 3,
-
               repeatType: "reverse",
             }}
           >
