1f1200af9034d2bf211878df8be02197dcf1639e
homto <homtoyat@gmail.com>
2025-03-09 22:01:44 +0800

    notification

--- src/app/(home)/stagger/page.tsx
+++ src/app/(home)/stagger/page.tsx
@@ -1,12 +1,20 @@
 "use client";
 import { AnimatePresence, motion } from "framer-motion";
-import { MenuIcon } from "lucide-react";
+import { HomeIcon, MenuIcon } from "lucide-react";
 import { useState } from "react";
 const menuItems = ["home", "about", "contact", "services"];
 export default function Motion() {
   const [open, setOpen] = useState(false);
   return (
     <AnimatePresence>
+      <button
+        className="rounded-2xl bg-amber-400 p-4 hover:cursor-pointer active:bg-amber-300/50"
+        onClick={() => {
+          setOpen(() => !open);
+        }}
+      >
+        <MenuIcon></MenuIcon>
+      </button>
       <motion.div
         className="w-16 bg-amber-400 shadow-2xl"
         animate={open ? "open" : "closed"}
@@ -14,54 +22,26 @@ export default function Motion() {
         variants={{
           open: {
             width: "10rem",
-            transition: {
-              staggerChildren: 0.4,
-              staggerDirection: 1,
-            },
+            transition: { staggerChildren: 0.1 },
           },
           closed: {
-            width: "4rem",
-            rotate: 360,
-            transition: {
-              staggerChildren: 0.1,
-              staggerDirection: -1,
-              duration: 0.4,
-            },
+            width: "0",
+            transition: {},
           },
         }}
       >
-        <button
-          className="rounded-2xl bg-amber-400 p-4 hover:cursor-pointer active:bg-amber-300/50"
-          onClick={() => {
-            setOpen(() => !open);
-          }}
-        >
-          <MenuIcon></MenuIcon>
-        </button>
-
         <div className="flex min-h-screen w-full flex-col items-start bg-amber-400">
           {menuItems.map((item) => (
             <AnimatePresence key={item}>
-              <div>
-                {/* {open && (
-                  <motion.div
-                    className="flex h-16 w-full items-center justify-center"
-                    variants={{
-                      open: { opacity: 1, x: 0 },
-                      closed: { opacity: 0, x: 100 },
-                    }}
-                  >
-                    <Clock10Icon></Clock10Icon>
-                  </motion.div>
-                )} */}
+              <div className="flex items-center justify-center gap-4 px-4">
                 <motion.div
-                  className="flex h-16 w-full items-center justify-center px-8"
+                  className="flex h-16 w-full"
                   variants={{
-                    open: { opacity: 1, x: 0 },
-                    closed: { opacity: 0, x: -20 },
+                    open: { opacity: 1, x: 0, display: "flex" },
+                    closed: { opacity: 0, x: -20, display: "none" },
                   }}
                 >
-                  {item}
+                  <HomeIcon className="block"></HomeIcon> {item}
                 </motion.div>
               </div>
             </AnimatePresence>
