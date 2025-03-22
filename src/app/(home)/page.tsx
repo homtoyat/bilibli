ff6d486b69a0fb9fdce710ed74177b36dc203a63
homto <homtoyat@gmail.com>
2025-03-22 19:46:19 +0800

    commit

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -60,7 +60,7 @@ export default function Page() {
               {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14].map((i) => {
                 return (
                   <div key={i}>
-                    <VideoItem index={i}></VideoItem>;
+                    <VideoItem index={i}></VideoItem>
                   </div>
                 );
               })}
