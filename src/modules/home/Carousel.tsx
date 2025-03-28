ebf9cb42d2ef73b1eb2906c50daa09a257647975
homto <homtoyat@gmail.com>
2025-03-28 21:01:52 +0800

    slider

--- src/modules/home/Carousel.tsx
+++ src/modules/home/Carousel.tsx
@@ -1,13 +1,17 @@
 import { AnimatePresence, motion } from "framer-motion";
-import { useState } from "react";
+import { ChevronLeft, ChevronRight } from "lucide-react";
+import { useRef, useState } from "react";
 
 interface Props {
   items: string[];
 }
 export function Carousel({ items }: Props) {
   const [[page, dir], setPage] = useState([0, 0]); // [当前页, 滑动方向]
-  const [isAnimating, setIsAnimating] = useState(false);
-  const paginate = (newDir: number) => {
+  const animating = useRef(false);
+  const paginate = async (newDir: number) => {
+    if (animating.current) {
+      return;
+    }
     setPage(([_page]) => [(_page + newDir + 3) % 3, newDir]);
   };
 
@@ -22,14 +26,14 @@ export function Carousel({ items }: Props) {
           exit={{ x: dir > 0 ? "-100%" : "100%", opacity: 1 }}
           className="absolute h-full w-full"
           transition={{ type: "tween", duration: 0.3 }}
-          onAnimationStartCapture={() => {
-            if (isAnimating) {
-              return;
-            } else {
-              setIsAnimating(true);
-            }
+          onAnimationComplete={(definition) => {
+            console.log("完成的动画定义:", definition);
+            // 输出：{ x: 100, opacity: 1 }
+            animating.current = false;
           }}
-          onAnimationEndCapture={() => setIsAnimating(false)}
+          onAnimationStart={() => {
+            animating.current = true;
+          }}
         >
           {/* eslint-disable-next-line @next/next/no-img-element */}
           <img
@@ -43,15 +47,15 @@ export function Carousel({ items }: Props) {
       {/* 导航按钮 */}
       <button
         onClick={() => paginate(-1)}
-        className="absolute top-1/2 left-4 -translate-y-1/2 rounded-full bg-white/30 p-2"
+        className="absolute top-1/2 left-4 -translate-y-1/2 cursor-pointer rounded-full bg-white/30 p-2 active:bg-white/60"
       >
-        ←
+        <ChevronLeft></ChevronLeft>
       </button>
       <button
         onClick={() => paginate(1)}
-        className="absolute top-1/2 right-4 -translate-y-1/2 rounded-full bg-white/30 p-2"
+        className="absolute top-1/2 right-4 -translate-y-1/2 cursor-pointer rounded-full bg-white/30 p-2 active:bg-white/60"
       >
-        →
+        <ChevronRight></ChevronRight>
       </button>
     </div>
   );
